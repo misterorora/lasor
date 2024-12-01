8b0cb9c346180631fac88f0236a593bf6a7679fd
jaysprograms <jason.piyasron@gmx.net>
2024-12-01 12:41:16 +0100

    Hover text red in navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -2,9 +2,9 @@
 	<div class="max-w-7xl mx-auto p-4 flex justify-between items-center">
 		<h1 class="text-xl font-bold text-white">Stellar Swords</h1>
 		<ul class="flex justify-center space-x-4 text-white">
-			<li><a href="/home" class="">Home</a></li>
-			<li><a href="/shop" class="">Shop</a></li>
-			<li><a href="/contact" class="">Contact</a></li>
+			<li><a href="/home" class="hover:text-red-500">Home</a></li>
+			<li><a href="/shop" class="hover:text-red-500">Shop</a></li>
+			<li><a href="/contact" class="hover:text-red-500">Contact</a></li>
 		</ul>
 	</div>
 </nav>
