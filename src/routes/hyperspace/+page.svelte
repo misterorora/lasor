23272b8660e9906168d34a13b2ffaf2c5b689ab8
paulstober20 <paulstober20@gmail.com>
2024-12-26 21:26:44 +0100

    Add audio on Landing page

--- src/routes/hyperspace/+page.svelte
+++ src/routes/hyperspace/+page.svelte
@@ -1,34 +1,47 @@
 <script>
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 
-	let isLoaded = false;
+	let audio;
 
 	onMount(() => {
 		const checkCanvas = setInterval(() => {
-			const canvas = document.querySelector("canvas");
+			const canvas = document.querySelector('canvas');
 			if (canvas) {
 				clearInterval(checkCanvas);
 				setupCanvas(canvas);
 			}
 		}, 100);
+
+		// Audio-Element erstellen und Pfad anpassen!!!
+		audio = new Audio('/HyperdriveSoundShort.mp3'); // Korrekter Pfad!
+		audio.preload = 'auto'; // Audio vorladen
 	});
 
 	function setupCanvas(canvas) {
-		canvas.addEventListener("click", () => {
-			canvas.dispatchEvent(new Event("mousedown"));
+		canvas.addEventListener('click', () => {
+			// Sound abspielen
+			if (audio) {
+				audio.currentTime = 0; // Zurück zum Anfang, falls bereits gespielt wird
+				audio.play();
+			}
+
+			canvas.dispatchEvent(new Event('mousedown'));
 
 			setTimeout(() => {
-				canvas.dispatchEvent(new Event("mouseup"));
+				canvas.dispatchEvent(new Event('mouseup'));
 			}, 3000);
 
 			setTimeout(() => {
+				if (audio) {
+					audio.pause();
+				}
 				goto('/home');
 			}, 5000);
 		});
 	}
 </script>
 
-<div class="center-text text-white" >
+<div class="center-text text-white">
 	Klicke zum Starten
 </div>
