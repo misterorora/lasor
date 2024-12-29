7fc528ef6ade0d4a82af793c7cfdc31b68ebd534
jaysprograms <jason.piyasron@gmx.net>
2024-12-29 20:15:23 +0100

    Ton hinzugefügt und Shop Coming Soon und Contact

--- src/lib/InterviewCard.svelte
+++ src/lib/InterviewCard.svelte
@@ -27,7 +27,7 @@
 <div class="container mx-auto py-10 space-y-10 text-white">
 
 	<!-- Section 2: Text links, Bild rechts -->
-		<div class="flex flex-col items-center md:items-start md:flex-row-reverse">
+		<div class="flex flex-col mt-40 items-center md:items-start md:flex-row-reverse">
 			<div
 				class={`md:w-1/2 flex justify-center p-20 mb-6 md:mb-0 transition-opacity duration-1000 ${
 	        isVisible ? "fade-in" : "opacity-0"
@@ -41,6 +41,9 @@
 					Unser Design ist einzigartig und wird dich begeistern. Wir haben uns von den besten Jedi-Meistern inspirieren lassen und
 					unsere Laserschwerter so gestaltet, dass sie nicht nur gut aussehen, sondern auch gut in der Hand liegen. Die wichtigsten
 					Fragen rund um unser Konzept beantwortet dir unser Interview mit dem Gründer.
+				</p>
+				<p class="mt-10 font-bold">
+					Bald hier zu kaufen!
 				</p>
 			</div>
 		</div>
